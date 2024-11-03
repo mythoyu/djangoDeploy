457ca7ea751cc425ef51956ca15022638aa4f070
mythoyu <mythoyu@gmail.com>
2024-11-03 21:56:46 +0900

    vercel설정파일

--- test_yu/settings.py
+++ test_yu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#3vzracwdwawtt21)+b$^etjbbs)280nc%z@&zq05i41!^!&^g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
