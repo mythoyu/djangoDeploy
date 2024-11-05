1a63cedef740235ccbe3a17c5cc16bef4a99cb4b
mythoyu <mythoyu@gmail.com>
2024-11-05 12:19:48 +0900

    1

--- post/urls.py
+++ post/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 from . import views
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # 관리자 페이지 경로 유지
@@ -30,5 +29,6 @@ urlpatterns = [
     path('', views.posts, name='home'),  # 루트 경로를 게시글 목록으로 지정
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
