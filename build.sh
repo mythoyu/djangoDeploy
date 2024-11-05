c36686d89420f3a1921cd0e36fcaff399027f36c
mythoyu <mythoyu@gmail.com>
2024-11-05 12:34:25 +0900

    1

--- build.sh
+++ build.sh
@@ -7,14 +7,21 @@ python3.10 pip install -U pip
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.10 -m pip install -r requirements.txt
+pip install -r requirements.txt
 # Make migrations
 echo "Making migrations..."
-python3.10 manage.py makemigrations post --noinput
-python3.10 manage.py migrate --noinput
+# python3.10 manage.py makemigrations post --noinput
+# python3.10 manage.py migrate --noinput
+python3.10 manage.py migrate 
+
 
 # Collect staticfiles
 echo "Collect static..."
-python3.10 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic
 
 echo "Build process completed!"
+
+# build_files.sh
+
+
+# make migrations
